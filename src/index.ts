705120908b3ca64909af60c956e845b391a369d8
lixiaoyang1992 <lixiaoyang2345@gmail.com>
2018-04-19 23:56:56 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export default function index(hours, minutes) {
+  console.log(123);
+  return 1;
+}
---
+++ test/test.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+const main = require("..");
+
+function test() {
+  assert.equal(main(), 1);
+}
+
+test();
