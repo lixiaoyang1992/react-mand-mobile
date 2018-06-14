2c7b27d24140a64ec3cb92ebc74fe1f1b09ad640
lixioayang1992 <lixiaoyang2345@gmail.com>
2018-06-14 10:38:34 +0800

    add dev doc

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-react mand mobile
+# 如何开发
+
+首先执行
+
+    npm run dev
+
+再执行
+
+    npm run devlop
+
+访问 http://localhost:8000/ ，修改 components 目录和 src 目录下的文件
--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,24 +1,24 @@
-import * as React from "react";
+import * as React from 'react'
 
-type ReactNode = React.ReactNode;
+type ReactNode = React.ReactNode
 
 export interface ButtonProps {
-  children?: ReactNode;
-  onClick?: Function;
+  children?: ReactNode
+  onClick?: Function
 }
 
 export interface ButtonState {}
 
 class Button extends React.Component<ButtonProps, ButtonState> {
   handleClick = (e: any) => {
-    const { onClick } = this.props;
-    onClick(e);
-  };
+    const { onClick } = this.props
+    onClick(e)
+  }
 
   render() {
-    const { children } = this.props;
-    return <button onClick={this.handleClick}>{children}</button>;
+    const { children } = this.props
+    return <button onClick={this.handleClick}>{children}</button>
   }
 }
 
-export default Button;
+export default Button
